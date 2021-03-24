95f28428fd082d63455e8b18296e8a5d774d1032
Niloo <niloofargst@gmail.com>
2021-03-24 09:39:23 +0100

    fixed

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,9 @@
+const firstName = (firstName) => {
+    return firstName.toUpperCase();
+}
+const lastName = (lastName) => {
+    return lastName.toLowerCase();
+}
+
+exports.firstName = firstName;
+exports.lastName = lastName;
